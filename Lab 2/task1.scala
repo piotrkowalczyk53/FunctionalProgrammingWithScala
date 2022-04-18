e246b2a0420c8341bc9a6c17d44fba9df43e22d1
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2022-04-18 03:03:04 +0200

    Upload code written during course

--- Lab 2/task1.scala
+++ Lab 2/task1.scala
@@ -1,1 +1,32 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(pi)
+    println(pi(pi))
+    println(pi(pi(pi)))
+    printarg(arg = "hello")
+    printarg()
+    println(repN(5, (x: Int) => 2*x , 1 ))
+  }
 
+  def pi() : Double = {
+    3.1415
+  }
+  
+  def pi(arguments: Double) : Double = {
+    arguments * pi()
+  }
+  
+  def printarg(arg: String) : Unit ={
+    println(arg)
+  }
+  
+  def printarg() : Unit = {
+    println("-")
+  }
+  
+  def repN(times: Int, fun: Int => Int, first: Int) : Int = {
+    if (times == 0)
+      return first
+    repN(times-1, fun, fun(first))
+  }
+}
