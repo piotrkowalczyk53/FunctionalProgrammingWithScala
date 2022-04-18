9967d21ddd50ebf82fead1d962e4b014a7ed8614
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2022-04-18 15:56:49 +0200

    Uploaded code written during the course

--- Lab 3/task1.scala
+++ Lab 3/task1.scala
@@ -1,1 +1,22 @@
+object counter {
+  def main(args: Array[String]): Unit = {
+    println(countCharacters(args.toList))
+  }
 
+def countCharacters(phrase: List[String]): Int = {
+  @scala.annotation.tailrec
+  def countLetters(word: String, counter: Int = 0) : Int = {
+    if(word.isEmpty)
+      return counter
+    countLetters(word.tail, counter + 1)
+  }
+  
+  @scala.annotation.tailrec
+  def countRec(words: List[String], counter: Int = 0) : Int = {
+    if(words.isEmpty)
+      return counter
+    countRec(words.tail, counter + countLetters(words.head))
+  }
+  countRec(phrase)
+  }
+}
