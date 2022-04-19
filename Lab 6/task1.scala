10f6b30053602e098baf53a311be4decf926e829
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2022-04-19 15:30:54 +0200

    Uploaded code written during the course

--- Lab 6/task1.scala
+++ Lab 6/task1.scala
@@ -1,1 +1,45 @@
+object Main 
+{
+  def main(args: Array[String]): Unit = {
+    val myPizza = new ThinDough with TomatoSauce with Mozarella with Ham
+    println(myPizza.name + myPizza.price)
+    val yourPizza = new ThinDough with TomatoSauce with Mozarella with Mushrooms
+    println(yourPizza.name + yourPizza.price)
+  }
 
+  abstract class PizzaTopping()
+  {
+    def name : String
+    def price : Int
+  }
+
+  class ThinDough extends PizzaTopping
+  {
+    override def name : String = " on thin dough "
+    override def price : Int = 10
+  }
+
+  trait TomatoSauce extends PizzaTopping
+  {
+    override abstract def name : String = " Tomato Sauce" + super.name
+    override abstract def price : Int = super.price + 1
+  }
+
+  trait Mozarella extends PizzaTopping
+  {
+    override abstract def name : String = " Mozarella" + super.name
+    override abstract def price : Int = super.price + 3
+  }
+
+  trait Ham extends PizzaTopping
+  {
+    override abstract def name : String = " Ham" + super.name
+    override abstract def price : Int = super.price + 5
+  }
+
+  trait Mushrooms extends PizzaTopping
+  {
+    override abstract def name :String = " Mushrooms" + super.name
+    override abstract def price : Int = super.price + 2
+  }
+}
