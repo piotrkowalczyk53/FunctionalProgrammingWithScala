8d109e7b2b8c858d05c5bc40ca7f66137deb54b8
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2022-04-26 00:49:11 +0200

    Uploaded code written during the course

--- Lab 12/task1.scala
+++ Lab 12/task1.scala
@@ -1,1 +1,29 @@
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.util.{Success, Failure, Random}
+import scala.concurrent.duration._
 
+object Main{
+  def main(args: Array[String]): Unit = {
+    val x = Random.alphanumeric
+    val rstr = (x take 100).toVector
+    val found = psearch(rstr, 'x')
+    found onComplete{
+      case Success(f) => println("found " + f )
+      case Failure(f) => println("not found" + f)
+    }
+  }
+  
+  def subSearch(data: Vector[Char], ans:Char):Char = {
+    if(data.contains(ans)) 
+      ans
+    else
+      '.'
+  }
+
+  def psearch(data: Vector[Char], ans: Char) : Future[List[Char]] = {
+    val divided = data.grouped(data.size/10).toList
+    val answer = for(e <- divided) yield Future{subSearch(e, ans)}
+    Future.sequence(answer)
+  }
+}
